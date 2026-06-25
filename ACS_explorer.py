9886d8a3f633b3f141c0e9137a7293d55c3a0a50
William Vaughen <wlvaughen@gmail.com>
2026-06-25 11:06:02 -0400

    "Importing initial acs explorer file and prepared acs data files"

---
+++ ACS_explorer.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+import time
+
+df = pd.read_csv("Processed ACS Data/acs_2024.csv")
+
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+
+x = st.sidebar.slider('x')
+st.write(x, 'squared is', x**2)
+
+st.text_input("Enter your name", key='name')
+st.session_state.name
+
+
+
+if st.sidebar.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    chart_data
+
+
+
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+
+option = st.sidebar.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected: ', option
+
+
+left_column, right_column = st.columns(2)
+# You can use a column just like st.sidebar:
+left_column.button('Press me!')
+
+# Or even better, call Streamlit functions inside a "with" block:
+with right_column:
+    chosen = st.radio(
+        'Sorting hat',
+        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+    st.write(f"You are in {chosen} house!")
+
+
+
+
+if "counter" not in st.session_state:
+    st.session_state.counter = 0
+
+st.session_state.counter += 1
+
+st.header(f"This page has run {st.session_state.counter} times.")
+st.button("Run it again")
